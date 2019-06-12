208a6e2bb7162261b8d1aaafa53b8661e327a4bd
agrusakov <agrusakov@gmail.com>
2019-06-13 00:29:35 +0300

    sql

---
+++ SQL/db work.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS work;
+USE work;
+CREATE TABLE IF NOT EXISTS employees (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT primary key,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+post VARCHAR(30) NOT NULL,
+salary INT NOT NULL);
+insert into employees (id, first_name, last_name, post, salary) values (null, 'ivan', 'ivanov', 'manager', 25);
+insert into employees (id, first_name, last_name, post, salary) values (null, 'sergey', 'bukin', 'driver', 35);
+insert into employees (id, first_name, last_name, post, salary) values (null, 'petr', 'komov', 'director', 45);
+insert into employees (id, first_name, last_name, post, salary) values (null, 'denis', 'blinov', 'manager', 20);
+insert into employees (id, first_name, last_name, post, salary) values (null, 'germagen', 'bitkov', 'seller', 20);
+select * from employees; 
+select * from employees where salary<30;
+select * from employees where post = 'manager' and salary<30;
+select last_name, post, salary from employees where salary<30;
+select last_name, post, salary from employees where post = 'manager' and salary<30;
